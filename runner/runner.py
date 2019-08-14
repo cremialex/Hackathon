2e7d7291788ecba0a8cb9fb1a8bdbd477fb6abe1
vicotr <victor@blanchier.com>
2019-08-14 21:10:12 +0100

    rfq

--- runner/runner.py
+++ runner/runner.py
@@ -1,6 +1,6 @@
 # Base Runner Code
 
-import client.client as client
+
 import rfq.rfq_sender as generator
 import runner.allocator as alloc
 import runner.calendar as cal
@@ -12,39 +12,50 @@ class Runner:
         self.clients = []
 
     def run(self):
+        import client.client as client
         print(self.current_day.get_current_time())
-
-        # Creating fake clients
-        client_baptiste = client.Client('Baptiste')
-        self.clients.append(client_baptiste)
 
         def answer_rfq_victor(incoming_rfq):
             if incoming_rfq[1] > 0:
                 return 3
             else:
                 return 2
+
+        def answer_rfq_baptiste(incoming_rfq):
+            return 5
+
+        # Creating fake clients
+        client_baptiste = client.Client('Baptiste', answer_rfq_baptiste)
+        self.clients.append(client_baptiste)
         client_victor = client.Client('Victor', answer_rfq_victor)
         self.clients.append(client_victor)
         # ----------------------
 
-        allocator = alloc.Allocator(self.clients)
-
-        rfq = generator.get_new_rfq()
-        print(rfq)
-
         self.current_day.set_begin_of_day()
         print(self.current_day.get_current_time())
+        RFQs = []
 
-        print(client_baptiste.name + ' answer is ' + str(client_baptiste.answer_rfq(rfq)))
-        print(client_victor.name + ' answer is ' + str(client_victor.answer_rfq(rfq)))
+        for i in range(9):
+            RFQs.append(generator.get_new_rfq())
 
-        print('Winner of the auction is ' + allocator.allocate_rfq(rfq).name)
+
+        for rfq in RFQs:
+            print(rfq)
+
+            allocator = alloc.Allocator(self.clients)
+
+            for client in self.clients:
+                print(str(client.name) + ' answer is ' + str(client.answer_rfq(rfq)))
+
+            print('Winner of the auction is ' + allocator.allocate_rfq(rfq).name)
+            print("\n")
 
         self.current_day.set_end_of_day()
         print(self.current_day.get_current_time())
 
-        client_baptiste.display_portfolio()
-        client_victor.display_portfolio()
+        for client in self.clients:
+            print(client.name + ' portfolio is:')
+            client.display_portfolio()
 
         self.current_day.set_next_business_day()
         print(self.current_day.get_current_time())
