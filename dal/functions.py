2ef43eb8bc448aac0fc62be3ba54324993ee99ef
Baptiste56 <batman_56@hotmail.fr>
2019-08-17 21:35:08 +0100

    code cleanup

--- dal/functions.py
+++ dal/functions.py
@@ -17,6 +17,7 @@ def get_working_days(year):
 
     return days
 
+
 def get_prices(list_of_tickers):
     df = pd.read_csv('data/data_new_list_wikipedia.csv', header=[0, 1], index_col=0, dayfirst=True, parse_dates=True)
     df = df[list_of_tickers]
@@ -38,24 +39,25 @@ def get_volumes(list_of_tickers):
 
 
 def get_volatilities(data, days):
-
     return data.rolling(days).std()
 
-def get_moving_average(data, days):
 
+def get_moving_average(data, days):
     return data.rolling(days).mean()
+
 
 def get_macd(data):
     exp1 = data.ewm(span=12, adjust=False).mean()
     exp2 = data.ewm(span=26, adjust=False).mean()
-    macd = exp1-exp2
+    macd = exp1 - exp2
 
     return macd
+
 
 def get_macd_signal_line(data):
     exp1 = data.ewm(span=12, adjust=False).mean()
     exp2 = data.ewm(span=26, adjust=False).mean()
-    macd = exp1-exp2
+    macd = exp1 - exp2
     exp3 = macd.ewm(span=9, adjust=False).mean()
 
     return exp3
--- rfq/rfq_sender.py
+++ rfq/rfq_sender.py
@@ -1,16 +1,6 @@
-#!/usr/bin/python
-
 import random
-from datetime import datetime
-from datetime import timedelta
-from random import randrange
 
 import dal.functions as dal
-import rfq.RFQ as RFQ
-
-
-def random_date(start, end):
-    return start + timedelta(seconds=random.randint(0, int((end - start).total_senconds())))
 
 
 def get_new_rfq():
@@ -18,31 +8,3 @@ def get_new_rfq():
     index_sp500 = random.randint(0, len(data))
     quantity = random.randint(-10000, 10000)
     return data[index_sp500], quantity
-
-
-def random_date(start, end):
-    # function that returns the random datetime between 2 quantities
-    random_hours = randrange(27)
-    date_random = start + timedelta(hours=random_hours)
-    return date_random
-
-
-def rdf_generator(time):
-    start_date = datetime.strptime(time, '%d/%m/%Y')
-    end_date = datetime.strptime('1/1/2019', '%d/%m/%Y')
-    random_hours = randrange(72)  # random within 3 days
-    rfq_time = start_date + timedelta(hours=random_hours)
-    print(rfq_time)
-    sym, qty = get_new_rfq()
-    print(sym)
-    return RFQ.RFQ(rfq_time, sym, qty)
-
-# print("example that print the RFQ attributes (random sym, date and qty)")
-#
-# testRFQ = rdf_generator("01/01/2018")
-# testRFQ2 = rdf_generator("01/01/2018")
-# testRFQ3 = rdf_generator("01/01/2018")
-#
-# print("RFQ 1: sym", testRFQ.sym, "qty: ", testRFQ.qty, ", date RFQ random range with in range: ", testRFQ.time, " ")
-# print("RFQ 2: sym", testRFQ2.sym, "qty: ", testRFQ2.qty, ", date RFQ random range with in range: ", testRFQ2.time, " ")
-# print("RFQ 3: sym", testRFQ3.sym, "qty: ", testRFQ3.qty, ", date RFQ random range with in range: ", testRFQ3.time, " ")
