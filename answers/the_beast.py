d0ef123c0137d37bce7e151c2291232d759a7de4
Baptiste56 <batman_56@hotmail.fr>
2019-08-17 21:16:10 +0100

    reformat the beast

--- answers/the_beast.py
+++ answers/the_beast.py
@@ -7,9 +7,7 @@ def answer_rfq(incoming_rfq):
     price_stock_hist = dal.get_prices([symbol])
     price_stock_hist_ret = price_stock_hist.pct_change() * trade_vol / abs(trade_vol)
 
-    vol = dal.get_volumes([symbol])
-
-    zdict = {
+    z_dict = {
         0.8: 1.0050000001,
         1: 1.000000001,
         -0.8: 0.99499999999,
@@ -19,6 +17,6 @@ def answer_rfq(incoming_rfq):
     rtn_z = ((price_stock_hist_ret.iloc[-1] - price_stock_hist_ret.iloc[-10:].mean()) / price_stock_hist_ret.iloc[
                                                                                         -60:].std()).values
 
-    rtn_mult = zdict[min(zdict.keys(), key=lambda x: abs(x - rtn_z))]
+    rtn_multiplier = z_dict[min(z_dict.keys(), key=lambda x: abs(x - rtn_z))]
 
-    return price_stock_hist.iloc[-1].values[0] * rtn_mult
+    return price_stock_hist.iloc[-1].values[0] * rtn_multiplier
