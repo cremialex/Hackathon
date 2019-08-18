5adeb497a66ce94665a74b4c7d0e1baeb9d815bc
Baptiste56 <batman_56@hotmail.fr>
2019-08-18 18:59:53 +0100

    rename attributes Client class

--- client/client.py
+++ client/client.py
@@ -3,34 +3,34 @@ from client.portfolio import Portfolio
 
 class Client:
     def __init__(self, name, function_rfq=lambda x: 0):
-        self.name = name
-        self.function_rfq = function_rfq
-        self.portfolio = Portfolio()
-        self.pnl = 0
-        self.is_bankrupt = False
+        self._name = name
+        self._function_rfq = function_rfq
+        self._portfolio = Portfolio()
+        self._pnl = 0
+        self._is_bankrupt = False
 
     def answer_rfq(self, incoming_rfq):
-        if self.is_bankrupt:
+        if self._is_bankrupt:
             return None
-        return self.function_rfq(incoming_rfq)
+        return self._function_rfq(incoming_rfq)
 
     def add_to_portfolio(self, position):
-        self.portfolio.add_position(position)
+        self._portfolio.add_position(position)
 
     def display_portfolio(self):
-        print(self.name + ' Portfolio is composed of: ')
-        self.portfolio.compute_inventory()
-        self.portfolio.show_inventory()
-        print(self.name + ' PnL is: ' + str(self.pnl))
+        print(self._name + ' Portfolio is composed of: ')
+        self._portfolio.compute_inventory()
+        self._portfolio.show_inventory()
+        print(self._name + ' PnL is: ' + str(self._pnl))
 
     def get_name(self):
-        return self.name
+        return self._name
 
     def get_portfolio(self):
-        return self.portfolio
+        return self._portfolio
 
     def adjust_pnl(self, trade_pnl):
-        self.pnl = self.pnl + trade_pnl
-        if not self.is_bankrupt and self.pnl < -1_000_000:
-            self.is_bankrupt = True
-            print(self.name + ' is now BANKRUPT')
+        self._pnl = self._pnl + trade_pnl
+        if not self._is_bankrupt and self._pnl < -1_000_000:
+            self._is_bankrupt = True
+            print(self._name + ' is now BANKRUPT')
