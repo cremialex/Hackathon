cb16d1772715de342ead40e0e0c6a5db9682ec8a
Baptiste56 <batman_56@hotmail.fr>
2019-08-18 19:26:02 +0100

    fix on random int generation bound in Rfq

--- rfq/rfq_sender.py
+++ rfq/rfq_sender.py
@@ -6,6 +6,6 @@ from rfq.rfq import Rfq
 
 def get_new_rfq():
     data = DalService.get_company_list()
-    index_sp500 = random.randint(0, len(data))
+    index_sp500 = random.randint(0, len(data) - 1)
     quantity = random.randint(-10000, 10000)
     return Rfq(data[index_sp500], quantity)
