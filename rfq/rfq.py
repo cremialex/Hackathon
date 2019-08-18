3670b8e17a447a5c2bf0741494a006d9c5d47fad
Baptiste56 <batman_56@hotmail.fr>
2019-08-18 11:53:29 +0100

    clean Rfq class

--- rfq/rfq.py
+++ rfq/rfq.py
@@ -1,10 +1,13 @@
 class Rfq:
     def __init__(self, sym, qty):
-        self.sym = sym
-        self.qty = qty
+        self._sym = sym
+        self._qty = qty
 
     def get_sym(self):
-        return self.sym
+        return self._sym
 
     def get_qty(self):
-        return self.qty
+        return self._qty
+
+    def __str__(self):
+        return ' '.join([self._sym, str(self._qty)])
