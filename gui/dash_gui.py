5bf1faf2e5dbb175bf1732466e77f3f811ada06f
vicotr <victor@blanchier.com>
2019-08-31 22:45:30 +0100

    reading graph from directory

--- gui/dash_gui.py
+++ gui/dash_gui.py
@@ -7,6 +7,7 @@ import datetime
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import csv
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -50,27 +51,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     html.Div(id='output-container-button',
              children='Enter the year for the RFQ to run'),
 
-##create dataframe that plot of each date the PnL
-    #header date, name each team,
-    # 2018.08.01 -20
-    dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'line', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'title': 'Analytics for the PnL over time',
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    ),
     dcc.Graph(id='pnl_graph'),
-
 ])
 
 
@@ -119,23 +100,30 @@ def read_clients_and_create_dataframe(value):
             data[name]= 0
 
     pnl_table = pd.DataFrame(data, index=[dateJan])
-    #test to see the chart
-    i=1
-    data2 = {}
-    dateJan2 = datetime.date(year=int(value), month=1, day=2).isoformat()
-    for file in os.listdir(directory_answers):
-        if file.endswith(".py"):
-            filename = os.fsdecode(file)
-            name = filename.split(".")[0]
-            data2[name]= 1+ i
-            i=i+1
-    print(data2)
-    print(pnl_table)
-    pnl_table.loc[dateJan2] = data2
-    #pnl_table.append(pd.Series(data2,index=dateJan2))
-    print(pnl_table)
-    print("hitting loop")
+
+    #read csv + plot
+    with open(os.getcwd() + '/../logs/PnlEvent.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        print(pnl_table)
+        for eventPnl in csv_reader:
+            date= datetime.datetime.strptime(eventPnl[0], '%Y%m%d').date()
+            print(date.isoformat() in pnl_table.index)
+            if date.isoformat() not in pnl_table.index:
+                print('here')
+                print(pnl_table.tail(1))
+                print(pnl_table.index[-1])
+                print('test')
+                data= pnl_table.loc[pnl_table.index[-1]]
+                print(data)
+                pnl_table.loc[date.isoformat()]=data
+                #pnl_table.iloc[-1]=#add new date in PnL
+                print('heer2')
+            pnl_table.at[date.isoformat(), eventPnl[2]] = float(pnl_table.at[date.isoformat(), eventPnl[2]]) + float(eventPnl[1])
+
     return pnl_table
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
