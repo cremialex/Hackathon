47fbe325edf21cb01fb9ebf711877c83ba577c4c
Baptiste56 <batman_56@hotmail.fr>
2020-01-23 19:35:31 +0000

    create Rfq service

--- rfq/rfq_sender.py
+++ rfq/rfq_sender.py
@@ -4,10 +4,25 @@ from dal.service import DalService
 from rfq.rfq import Rfq
 
 
-def get_new_rfq():
-    data = DalService.get_company_list()
-    index_sp500 = random.randint(0, len(data) - 1)
-    price = DalService.get_prices(data[index_sp500]).iloc[-1].values[0]
-    num_of_shares = int(500000/price)
-    quantity = random.randint(-num_of_shares, num_of_shares)
-    return Rfq(data[index_sp500], quantity)
+class RfqService:
+    class __RfqService:
+        def __init__(self, param):
+            self.param = param
+
+        def generate_rfq(self):
+            data = DalService.get_company_list()
+            index_sp500 = random.randint(0, len(data) - 1)
+            price = DalService.get_prices(data[index_sp500]).iloc[-1].values[0]
+            num_of_shares = int(self.param / price)
+            quantity = random.randint(-num_of_shares, num_of_shares)
+            return Rfq(data[index_sp500], quantity)
+
+    instance = None
+
+    def __init__(self):
+        if not RfqService.instance:
+            RfqService.instance = RfqService.__RfqService(500000)
+
+    @staticmethod
+    def generate_rfq():
+        return RfqService.instance.generate_rfq()
